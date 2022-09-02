08fdb1d557324eaa3554e0a79d62d1b0fbd23c7a
laila kamel <laila_kamel@hotmail.com>
2022-09-02 14:08:31 +1000

    trying again

--- MyCustomReporter.js
+++ MyCustomReporter.js
@@ -1,11 +1,24 @@
-// class MyCustomReporter{
-//     constructor(globalConfig, options){
-//         this.globalConfig=globalConfig;
-//         this.options=options;
-//     }
-//     onRunComplete(vontexts, results){
-//         console.log("Custom reporter output:");
-//         console.log("Options:", this.options);
-//         console.log("Failed tests:", results.numFailedTests);
-//     }
-// }
+const { default: axios } = require("axios");
+
+class MyCustomReporter{
+    constructor(globalConfig, options){
+        this.globalConfig=globalConfig;
+        this.options=options;
+    }
+    onRunComplete(contexts, results){
+        console.log("Custom reporter output:");
+        console.log("Options:", this.options);
+        console.log("Failed tests:", results.numFailedTests);
+        axios({
+            url:"https://formspree.io/f/xlevgrzj",
+            method: 'post',
+        headers: {
+          'Accept': 'application/json'
+        },
+        data: results
+      }).then((response) => { console.log(response); })
+      
+    }
+}
+
+module.exports=MyCustomReporter;
