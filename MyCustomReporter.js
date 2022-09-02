0e6f2ab727dca5ee587f5c1f54174292908349ec
laila kamel <laila_kamel@hotmail.com>
2022-09-02 13:22:13 +1000

    trying again

---
+++ MyCustomReporter.js
@@ -0,0 +1,11 @@
+class MyCustomReporter{
+    constructor(globalConfig, options){
+        this.globalConfig=globalConfig;
+        this.options=options;
+    }
+    onRunComplete(vontexts, results){
+        console.log("Custom reporter output:");
+        console.log("Options:", this.options);
+        console.log("Failed tests:", results.numFailedTests);
+    }
+}
